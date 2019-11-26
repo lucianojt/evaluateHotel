7aae18569a925e832e549cbd79e7a15b7f254354
jidapa tassanakul <onjidapa@windowslive.com>
2019-11-26 10:52:22 +0700

    editSend

--- send.php
+++ send.php
@@ -1,5 +1,6 @@
 <?php
 ob_start();
+if(isset($_POST['submit'])){
 include("database/database.php"); 
 $connection = mysqli_connect($localhost,$username,$pass,$database);
 mysqli_set_charset($connection,'utf8');
@@ -7,7 +8,7 @@ mysqli_set_charset($connection,'utf8');
 <!doctype html>
 <html lang="en">
   <head>
-    <title>Title</title>
+    <title>send</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
@@ -24,44 +25,80 @@ background-color: #FAFAFA;
 </style>
 <div class="container">
 <?php
-var_dump($_POST);
+//var_dump($_POST);
 
-
-// $on = $_POST["choose1"];
-// $two = $_POST["choose2"];
-// $three = $_POST["choose3"];
-
-
-$q = 1;
-while($q<=16){
-
-    $i=1;
-    while($i<=5){
-    $sql = "SELECT counts FROM vote WHERE id_detail = '$q' AND score = '$i' ";
-    $result = mysqli_query($connection,$sql);
-    $row = mysqli_fetch_assoc($result);
-    if($row['counts'] == 0){
-
-        if($_POST["choose1"] == $i){
-        $sql = "UPDATE vote SET counts = 1 WHERE id_detail = '$q' AND score = '$i'";
+$sql2 = "SELECT * FROM vote";
+$result2 = mysqli_query($connection,$sql2);
+if(!$result2){
+    ?>
+    <br>
+    <div class="alert alert-danger" role="alert">
+    เกิดข้อผิดพลาดบางอย่าง 
+    </div>
+    <?php
+}else{
+    $q = 1;
+    while($q<=16){
+        $i=1;
+        while($i<=5){
+        $sql = "SELECT counts FROM vote WHERE id_detail = '$q' AND score = '$i' ";
         $result = mysqli_query($connection,$sql);
-        }
-
-    }else{
-        if($_POST["choose1"] == $i){
-        
-            $k = 1;
-            $k += $row['counts'];
-            $sql = "UPDATE vote SET counts = $k WHERE id_detail = '$q' AND score = '$i'";
+        $row = mysqli_fetch_assoc($result);
+        if($row['counts'] == 0){
+            if($_POST["choose".$q] == $i){
+            $sql = "UPDATE vote SET counts = 1 WHERE id_detail = '$q' AND score = '$i'";
             $result = mysqli_query($connection,$sql);
+            }
+        }else{
+            if($_POST["choose".$q] == $i){
+                $k = 1;
+                $k += $row['counts'];
+                $sql = "UPDATE vote SET counts = $k WHERE id_detail = '$q' AND score = '$i'";
+                $result = mysqli_query($connection,$sql);
+            }
         }
+        $i++;
+        }
+    $q++;
     }
-    $i++;
-    }
-$q++;
 }
 
 
+$sql2 = "SELECT * FROM voteoption";
+$result2 = mysqli_query($connection,$sql2);
+if(!$result2){
+    ?><br>
+    <div class="alert alert-danger" role="alert">
+    เกิดข้อผิดพลาดบางอย่าง 
+    </div>
+    <?php
+}else{
+    $qq = array('Y','N');
+    $x = 1;
+    while($x<=5){
+        $n = 0;
+        while($n<=1){
+            $sql = "SELECT counts FROM voteoption WHERE opinion_id = '$x' AND options = '$qq[$n]' ";
+            $result = mysqli_query($connection,$sql);
+            $row = mysqli_fetch_assoc($result);
+            if($row['counts'] == 0){
+                if($_POST['opin'.$x] == $qq[$n]){
+                    $sql = "UPDATE voteoption SET counts = 1 WHERE opinion_id = '$x' AND options = '$qq[$n]'";
+                    $result = mysqli_query($connection,$sql);
+                }
+            }else{
+                if($_POST['opin'.$x] == $qq[$n]){
+                    $kk = 1;
+                    $kk += $row['counts'];
+                    $sql = "UPDATE voteoption SET counts = $kk WHERE opinion_id = '$x' AND options = '$qq[$n]'";
+                    $result = mysqli_query($connection,$sql);
+                }
+            }
+        $n++;
+        }
+    $x++;
+    }  
+}
 ?>
 </div>
     <!-- Optional JavaScript -->
@@ -72,5 +109,8 @@ $q++;
   </body>
 </html>
 <?php
+}else{
+    header("location:index.php"); 
+}
 ob_end_flush(); 
 ?>
